ff07ab023b655ca6685ad92b9880291ee47b6ac4
Balaram <balarammukhi245@gmail.com>
2025-10-24 19:17:39 +0000

    variables

---
+++ 01_basics/variabels.js
@@ -0,0 +1,13 @@
+const accountId = 11111
+let accountEmail = "balartam@gmail.com"
+var accountPassword = "123456"
+accouuntCity = "Jalesware"
+
+// accountId = 2 // const can not be changed 
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accouuntCity])
