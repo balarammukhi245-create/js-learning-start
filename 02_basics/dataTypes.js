cd475f6b9437ea1a216c07828d0deb57fad43702
Balaram <balarammukhi245@gmail.com>
2025-10-25 20:15:27 +0000

    dataTypes

---
+++ 02_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"// treat all js code as newer version
+
+// alert (3+3) // we are using nodejs, not browser 
+
+//in jS data types are divided into two main categories
+//1=> Premitive 
+/*
+1. string= "Balaram"
+2. Number = 12345
+3. Bigint = 
+4. Boolean - true/false
+5. Undefind = undefined
+6. Null = like a Object
+*/
+
+let a = "balaram"
+let b= 12
+let c= true
+let d = undefined
+let e = null
+
+console.log(typeof a)
