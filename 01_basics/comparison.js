7728ad7e46ea72c272332ae8ae57c8152db0bd97
Balaram <balarammukhi245@gmail.com>
2025-10-30 18:01:32 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,13 @@
+// console.log("2" >1);// true
+// console.log("02" >1); //true
+
+
+// console.log(null >0); //false 
+// console.log(null == 0);// false
+// console.log(null >= 0); // true is is first convert to number as 0  then it compare so it is ture
+
+// console.log(undefind > 0); //false 
+// console.log(undefind == 0); //false
+// console.log(undefind >= 0); //false
+
+// === strict check it is check by data type and value 
