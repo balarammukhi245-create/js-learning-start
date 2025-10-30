8fad6ff31b21b7103375e4fe8b5793f94037a366
Balaram <balarammukhi245@gmail.com>
2025-10-30 17:32:58 +0000

    conversion and operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,69 @@
+// let score = 33
+
+// console.log(typeof(score));
+// number
+// let score = "33"
+// console.log(typeof score);
+
+// if we convert into number so we see this type of conversion in  js
+// "33" => 33
+// "3abc" => NaN
+// true => 1 ; false=>0
+
+// let isLooggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLooggedIn);
+// console.log(booleanIsLoggedIn);
+// 1=> true ; 0=> false
+// "" => false
+//"balaram" => true
+
+// let number = 33
+// let inString = String(number)
+// console.log(number);
+// console.log(typeof inString);
+
+// every time if convert somthing to somthing then we can use this type like Number,String,Boolean,etc.. the starting letter of conversion need to Capital letter 
+
+// OPERATION 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+// let str = "hello"
+// let str2 = " Balaram"
+
+// let str3 = str + str2
+// console.log(str3); //hello balaram
+
+// console.log("1"+"2") // 12
+// console.log(1+"2") //12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2") //32
+// if ther is staring is string so all are convert into string
+
+// if the starting is Number and others are string so the starting nuber are add and othe same as string 
+
+// console.log(true) // true
+// console.log(+true) // 1
+// // console.log(-true)// error
+// console.log(+"")//0
+
+// let num,num2,num3
+// num = num2=num3= 2+2
+// console.log(num)
+
+//prefix,postfix
+
+// let number = 11
+// // number++ postfix
+// // ++number prefix
+// console.log(number) // 12
