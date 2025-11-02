0ded960f58eef3cc6f0c242e86445bf7f38630f7
Balaram <balarammukhi245@gmail.com>
2025-11-02 16:04:58 +0000

    object

---
+++ 02_basics/object.js
@@ -0,0 +1,13 @@
+// singleton
+
+// object literals
+
+const jsUser = {
+    name:"Balaram",
+    myNumber: 111122233,
+    location:"Balasore",
+    gmail:"balaram@111.gmail.com"
+}
+
+console.log(jsUser.gmail);
+console.log(jsUser["gmail"]);
