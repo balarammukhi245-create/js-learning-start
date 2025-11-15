963074b0a686063d4d1d49018552064249d588aa
Balaram <balarammukhi245@gmail.com>
2025-11-15 20:41:22 +0000

    while and do-while loop

---
+++ 04_iteration/two.js
@@ -0,0 +1,25 @@
+//while do-while loop
+let index = 0
+// while (index<= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index= index +2 
+    
+// }
+
+// let myArray = ["shaktiman" , "balbeer", "sonpari"]
+// let arr = 0
+// while (arr< myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+
+//     arr = arr+1
+    
+// }
+
+// let score = 1
+let score = 11 
+//it  execute first then he check the condition
+do {
+    console.log(`Score is ${score}`);
+    // score = score+1
+    score++
+} while (score<=10);
