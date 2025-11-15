57d3bbc376b85bae61a9818953c4f5266fc045f5
Balaram <balarammukhi245@gmail.com>
2025-11-15 22:52:35 +0000

    filter

---
+++ 04_iteration/six.js
@@ -0,0 +1,22 @@
+const coding = ["js", "python", "java", 'cpp', "ruby"]
+// const values = coding.forEach( (item)=>{
+//     return item
+// })
+//  if we use foreach it does't return the value 
+// console.log(values);
+
+const myNumber = [1,2,3,4,5,6,7,8,9,10]
+
+const values = myNumber.filter((num)=>{
+    return num>4
+})
+// console.log(values);
+
+// const newNumber= []
+// myNumber.forEach((num)=>{
+//     if(num>4){
+//         newNumber.push(num)
+//     }
+// })
+
+// console.log(newNumber);
