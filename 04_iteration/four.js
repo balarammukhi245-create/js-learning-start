c5728bf5e2740af5537c594d299a8cd0d413801a
Balaram <balarammukhi245@gmail.com>
2025-11-15 21:25:41 +0000

    for in loop

---
+++ 04_iteration/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'JavaScript',
+    cpp: "c++",
+    rb: "Ruby",
+    swift : "Swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} is shortcut of ${myObject[key]}`);
+// }
+
+const myArray = ["js","python","cpp","java"]
+
+// for (const key in myArray) {
+//     console.log(myArray[key]);
+// }
+
+// const map = new Map()
+// map.set("Odisha","OD")
+// map.set("AndhraPradesh","AP")
+// map.set("Dilhi","DL")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+//for object we can use for in loop but in array we can use for of loop 
